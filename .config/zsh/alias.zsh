c4e393e7ba8a4dc3b60f181828ba5691ed5be013
Aditya Ghidora <adityaghidora@skylarkdrones.com>
2024-01-24 10:49:45 +0530

    Major overhaul
    Adds -
    	polybar config.ini as I like it
    	aliases
    	transmission settings.json
    	Updated the readme
    
    Removes -

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,22 +1,37 @@
+# config
+alias zshcfg="vim ~/.config/zsh/.zshrc"
+alias zshsrc="source ~/.config/zsh/.zshrc"
+alias termcfg="vim ~/.config/alacritty/alacritty.toml"
+alias alscfg="vim ~/.config/zsh/alias.zsh"
+alias kbcfg="vim ~/.config/sxhkd/sxhkdrc"
+
+# system
 alias ll="ls -l"
 alias lal="ls -al"
-alias zshcfg="nvim ~/.config/zsh/.zshrc"
-alias zshsrc="source ~/.config/zsh/.zshrc"
-alias pac="sudo pacman"
-alias termcfg="nvim ~/.config/alacritty/alacritty.yml"
-alias aliascfg="nvim ~/.config/zsh/alias.zsh"
-alias tsm="transmission-remote"
-alias vifmrc="nvim ~/.config/vifm/vifmrc"
+alias sa="cat ~/.config/zsh/alias.zsh"
+alias acfs="auto-cpufreq --stats"
+alias mrs="sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
+
+# systemctl
+alias sc="sudo systemctl"
+alias ss="systemctl status"
+
+# pacman
+alias pc="sudo pacman"
+alias pf="pacman -Ss"
+
+# docker
+alias d="docker"
+alias dc="docker-compose"
+
+# git 
 alias g="git"
 alias gs="git status"
-alias gl="git log | less"
-alias gaa="git add -A"
+alias gc="git checkout"
+alias gb="git branch"
+alias gr="git rebase"
+alias gl="git log"
+alias glo="git log --oneline"
+alias ga="git add"
 alias gcm="git commit"
 alias gp="git push"
-alias ptop="sudo powertop"
-alias acf="auto-cpufreq"
-alias acfs="auto-cpufreq --stats"
-alias mirrorsync="sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
-alias sa="cat ~/.config/zsh/alias.zsh"
-alias p="pnpm"
-alias px="pnpm dlx"
--- .zshenv
+++ .zshenv
@@ -1,16 +1,14 @@
 export ZDOTDIR="$HOME/.config/zsh"
-export SPACEVIMDIR="$HOME/.config/spacevim"
 export BROWSER=brave
 export READER=zathura
 export KEYTIMEOUT=1
-export EDITOR="nvim"
-export SYSTEMD_EDITOR="nvim"
+export EDITOR="vim"
+export SYSTEMD_EDITOR="vim"
 export TERMINAL="alacritty"
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
-export ANALYZER_STATE_LOCATION_OVERRIDE="$XDG_DATA_HOME/dartServer"
 export GTK2_RC_FILES="$HOME/.config/gtk-2.0/gtkrc-2.0"
 export GNUPGHOME="$XDG_DATA_HOME/gnupg"
 export WGETRC="$HOME/.config/wgetrc"
@@ -19,17 +17,5 @@ export PUB_CACHE="$XDG_CACHE_HOME/pub"
 export LESSHISTFILE="-"
 export ERRFILE="/dev/null"
 
-export ANDROID_HOME=/1tb/bin
-export PATH=$ANDROID_HOME/cmdline-tools/tools:$PATH
-export PATH=$ANDROID_HOME/cmdline-tools/tools/bin:$PATH
-export PATH=$ANDROID_HOME/platform-tools:$PATH
-export PATH=$ANDROID_HOME:$PATH
-
-export FLUTTER=$ANDROID_HOME/flutter
-export PATH=$FLUTTER/bin:$PATH
-
-export CARGO_HOME=$HOME/.cache/cargo
-export RUSTUP_HOME=$HOME/.cache/rustup
-export PATH=$PATH:"$HOME/.local/bin"
-export PATH=$PATH:"$HOME/.cache/cargo/bin"
-. "$HOME/.cache/cargo/env"
+export PATH=$PATH:"$HOME/.local/bin/"
+. "$HOME/.cargo/env"
